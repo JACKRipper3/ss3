53607990724c5ca58c61604401c7b10802db5f0c
JACKRipper3 <157270715+JACKRipper3@users.noreply.github.com>
2024-11-26 11:51:31 +0700

    fff

---
+++ kokko/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "my name is apisit jariut";
+string student_id = " id: 673450042-9";
+Console.WriteLine("Hello, World!");
+Console.WriteLine("my name is apisit jariut" + name);
+Console.WriteLine("my student id: 673450042-9" + student_id);
